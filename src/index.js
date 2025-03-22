c12037fbf0c0d0b542766082e5d003206db212c9
marvin-design <marvindavid649@gmail.com>
2025-03-22 10:09:38 +0300

    adding event listiner

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 // Your code here
+document.addEventListener("DOMContentLoaded", () => {
+    const baseURL = "http://localhost:3000/characters";
+    const characterBar = document.querySelector("#character-bar");
+    const detailedInfo = document.querySelector("#detailed-info");
+    const votesForm = document.querySelector("#votes-form");
+    const votesInput = document.querySelector("#votes");
+    const resetButton = document.querySelector("#reset-btn");
+    const addCharacterForm = document.querySelector("#character-form");
